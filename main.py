785541b7255b45bce9ff0ba47d1522f192ae1c3a
tiny_bear <rebeccawilliams9941@gmail.com>
2023-12-06 01:14:48 +1100

    :construction: Add character biography generator page.

--- main.py
+++ main.py
@@ -21,22 +21,30 @@ def home():
 def page_one():
     st.title('🧠 Thought Checker')
     st.write("Enter a journal entry, and this program will auto-detect unhelpful thinking patterns (cognitive distortions) in your thinking patterns, so you can focus on the more helpful reframing part.")
+    st.write("🛠️ OpenAI (GPT-3.5-turbo, function calling), Streamlit")
 
 def page_two():
     st.title('🪖 Interview Analyser')
     st.write("Upload interview transcripts, and this program will pull out direct quotes from the transcripts related to any question you want.")
+    st.write("🛠️ OpenAI (GPT-3.5-turbo, function calling), Streamlit")
     st.success("⏰ Saved 30 hours of sentiment analysis, so insights could be actioned faster")
     testimonial('Tam Asaad - CEO of Foura', "Quote")
 
 def page_three():
     st.title('🐼 Pitch Panda')
     st.write("A collaborative project with [Stan](https://www.linkedin.com/in/stanleaf?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAADRVb2QB43kcXPLI2AI7fGY09X8uBpCfO8k&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3Bx%2FVwbXzhRCOLCLq6CnlpFw%3D%3D), combining his IOT superpowers with my GenAI skills. We developed this robotic plush panda you can have a two-way conversation with.")
+    st.write("🛠️ OpenAI (GPT-3.5-turbo, function calling), Eleven Labs, Raspberry Pi")
     st.success("🏆 Winner of the [People's Choice Award at Fishburners young entrepreneur pitch night 2023](https://www.linkedin.com/posts/fishburners_entrepreneurs-genz-ai-activity-7120150734930673664-v_Wq)")
     testimonial('Martin Karafilis - CEO of Fishburners', "👏🏼 People's Choice: 🦕 Becca Williams, Founder of fairylights.ai, in a landslide victory after the most captivating display of generative-AI + tech in action we've ever seen!", 'images/testimonials/martin-karafilis.jpeg')
 
 def page_four():
     st.title('📈 Ask Your Spreadsheets')
     st.write("Upload your spreadsheets and ask questions in plain english. This program will auto-generate and execute SQL queries to retrieve the data needed to answer your question.")
+
+def page_five():
+    st.title('🧙‍Character Profiles')
+    st.write("Leverage scraped data in a spreadsheet for multiple characters/places to generate long-text profile biographies for them.")
+    st.success("🚀 Increased bookings made to specialist platform by 300% and organic traffic by 350%")
 
 # Categories sidebar
 with st.sidebar:
@@ -52,7 +60,7 @@ with st.sidebar:
 
     openai_api_key = st.text_input(" ", type="password", placeholder="Enter your OpenAI API key")
 
-    page = st.radio("Choose a project", ["Home", "#1. Thought Checker", "#2. Interview Analyser", "#3. Pitch Panda 🏆", "#4. Ask Your Spreadsheets"])
+    page = st.radio("Choose a project", ["Home", "#1. Thought Checker", "#2. Interview Analyser", "#3. Pitch Panda 🏆", "#4. Ask Your Spreadsheets", "#5. Generate character profiles"])
 
 if page == "Home":
   home()
@@ -64,3 +72,5 @@ elif page == "#3. Pitch Panda 🏆":
   page_three()
 elif page == "#4. Ask Your Spreadsheets":
   page_four()
+elif page == "#5. Generate character profiles":
+  page_five()
