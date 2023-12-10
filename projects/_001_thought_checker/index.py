cd6a7dae23ca23f0d7cddf0d40b5e2737234a08f
tiny_bear <rebeccawilliams9941@gmail.com>
2023-12-10 21:50:54 +1100

    :construction: Highlight cognitive distortions.

--- projects/_001_thought_checker/index.py
+++ projects/_001_thought_checker/index.py
@@ -20,43 +20,51 @@ def get_journal_entry():
     if form_submission:
         return journal_text
 
+
+def highlight_text(journal_entry, quotes):
+    for quote in quotes:
+        journal_entry = journal_entry.replace(quote, f"<span style='border: solid black 1px; border-radius: 5px; padding: 0px 5px; background: #31333F; color: white; hover: cursor;'>{quote}</span>")
+
+    return journal_entry
+
+
 def process_journal_entry(journal_text):
-      info_placeholder = st.empty()
+    info_placeholder = st.empty()
 
-      info_placeholder.info("(1/2) Identifying all thinking patterns in your journal entry...")
-      cognitive_distortions = identify_cognitive_distortions(journal_text)
-      info_placeholder.info("(2/2) Explaining and reframing...")
-      distortions = categorise_cognitive_distortions(cognitive_distortions.get("quotes"))
-      distortions_by_category = {}
-      info_placeholder.empty()
+    info_placeholder.info("(1/2) Identifying all thinking patterns in your journal entry...")
+    cognitive_distortions = identify_cognitive_distortions(journal_text)
+    info_placeholder.info("(2/2) Explaining and reframing...")
+    distortions = categorise_cognitive_distortions(cognitive_distortions.get("quotes"))
+    distortions_by_category = {}
+    info_placeholder.empty()
 
-      for pattern in distortions.get("thinking_patterns"):
-          category = pattern.get("thinking_pattern")
-          quote = pattern.get("quote")
-          explanation = pattern.get("explanation")
-          reframe = pattern.get("reframe")
+    for pattern in distortions.get("thinking_patterns"):
+        category = pattern.get("thinking_pattern")
+        quote = pattern.get("quote")
+        explanation = pattern.get("explanation")
+        reframe = pattern.get("reframe")
 
-          if category not in distortions_by_category:
-              distortions_by_category[category] = []
-          distortions_by_category[category].append((quote, explanation, reframe))
+        if category not in distortions_by_category:
+            distortions_by_category[category] = []
+        distortions_by_category[category].append((quote, explanation, reframe))
 
-      # Sort the categories by the number of entries (in descending order)
-      sorted_categories = sorted(distortions_by_category.items(), key=lambda x: len(x[1]), reverse=True)
+    # Sort the categories by the number of entries (in descending order)
+    sorted_categories = sorted(distortions_by_category.items(), key=lambda x: len(x[1]), reverse=True)
 
-      # Display the categories in sorted order
-      for category, entries in sorted_categories:
-          with st.expander(f"{category} (x {len(entries)})"):
-              for entry in entries:
-                  st.markdown(f"- **\"{entry[0][0].upper() + entry[0][1:]}\"** - {entry[1]} \n<span style='background: #C7F5F0;'>{entry[2]}</span>", unsafe_allow_html=True)
-
-
-
+    # Display the categories in sorted order
+    for category, entries in sorted_categories:
+        with st.expander(f"{category} (x {len(entries)})"):
+            for entry in entries:
+                st.markdown(
+                    f"- **\"{entry[0][0].upper() + entry[0][1:]}\"** - {entry[1]} \n<span style='background: #C7F5F0;'>{entry[2]}</span>",
+                    unsafe_allow_html=True)
 
 
 def thought_checker():
     st.title('🧠 Thought Checker')
-    st.write('Enter a journal entry, and this program will auto-detect unhelpful thinking patterns (cognitive distortions) that are present in your entry, so you can focus on the more helpful reframing part.')
+    st.write(
+        'Enter a journal entry, and this program will auto-detect unhelpful thinking patterns (cognitive distortions) that are present in your entry, so you can focus on the more helpful reframing part.')
     journal_text = get_journal_entry()
     if journal_text:
-      st.markdown(f"<div style='padding: 20px 20px 10px 20px; border-radius: 5px; background: #F0F2F6'>{journal_text}</div>", unsafe_allow_html=True)
-    #process_journal_entry(journal_text)
+        distortions = identify_cognitive_distortions(journal_text).get("quotes")
+        st.markdown(f"<div style='padding: 20px 20px 10px 20px; border-radius: 5px; background: #F0F2F6'>{highlight_text(journal_text, distortions)}</div>",unsafe_allow_html=True)
