d2d236cb86ba699c7208ef7bc35b70c8d680eeb4
tiny_bear <rebeccawilliams9941@gmail.com>
2023-12-25 15:32:36 +1100

    :chart_with_upwards_trend: Update event descriptions to better explain.

--- projects/_001_thought_checker/event_tracking.py
+++ projects/_001_thought_checker/event_tracking.py
@@ -5,7 +5,7 @@ def track_text_input():
     if 'text_entered' not in st.session_state:
         mp = Mixpanel(st.secrets["mixpanel"]["token"])
         mp.track(st.session_state['session_id'], 'Text Input', {
-            'event': 'Text Entered in Journal Field',
+            'event': 'Clicked on or scrolled journal entry field',
             'page_name': 'Thought Checker'
         })
         st.session_state['text_entered'] = True
@@ -15,14 +15,14 @@ def track_journal_entry_input(journal_text, default_journal_entry):
     mp = Mixpanel(st.secrets["mixpanel"]["token"])
     if journal_text != default_journal_entry:
         mp.track(st.session_state['session_id'], 'Text Input', {
-            'event': 'Journal Entry Input',
+            'event': 'Wrote own journal entry',
             'page_name': 'Thought Checker',
             'type': 'custom',
             'word_count': len(journal_text.split())
         })
     else:
         mp.track(st.session_state['session_id'], 'Text Input', {
-            'event': 'Journal Entry Input',
+            'event': 'Used demo journal entry',
             'page_name': 'Thought Checker',
             'type': 'demo',
             'word_count': len(journal_text.split())
