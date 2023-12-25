dde905a3692d2bbe538d2cea21c85f5613c611e5
tiny_bear <rebeccawilliams9941@gmail.com>
2023-12-26 04:43:19 +1100

    :construction: Add highlights.

--- pages/home/index.py
+++ pages/home/index.py
@@ -1,9 +1,12 @@
 import streamlit as st
 def home():
     st.title("🚀 100 GenAI projects 🤖")
-    st.write("I can't wait to explore what's possible with generative AI with you by shipping 100 happy and helpful use-cases. Every project shipped here has solved a problem for at least one real person. Try them out for yourself, and [subscribe for the newest possibilities](https://fairylightsai.substack.com/) 💌🥰")
+    st.write("I can't wait to explore what's possible with generative AI with you by shipping 100 happy and helpful use-cases. Every project shipped here has solved a problem for at least one real person.")
+    st.write("Try them out for yourself, and [subscribe for the newest possibilities](https://fairylightsai.substack.com/) 💌🥰")
 
-    with st.expander("🥰 See Timeline of Magical Milestones"):
+    st.subheader("✨Magical Moments")
+    st.warning("- 🏆 I co-won the [people's choice award at Fishburners Young Entrepreneur Pitch Night](https://www.linkedin.com/posts/martinkarafilis_entrepreneurs-genz-ai-activity-7120197635935768576-mG7E) for a robotic plush panda you can have a two-way convo with (collab project with a friend who did the IOT and I did the GenAI). \n - 🚀 One of my proprietry GenAI use-cases (specialist profile generator from scraped data) led to a 350% in organic traffic and 300% increase in patient bookings for a health startup in a single month. \n - 👷 I became a community advisor at [Builders club](https://www.linkedin.com/company/the-builders-club-ai/), and spearhead the [Humans of Builders club series](https://builder-club.beehiiv.com/).")
+    with st.expander("🥰 See full timeline of magical milestones!"):
             st.markdown("**Dec 2023**")
             st.markdown("- 🎉 Launched this 100 GenAI project site with first use-case.")
 
@@ -12,7 +15,7 @@ def home():
 
             st.markdown("**Oct 2023**")
             st.markdown("- 👷‍ Became a community advisor at [AI Builders Club](https://www.linkedin.com/company/the-builders-club-ai/) which does weekly hackathons and opens up a world of opportunities for fellow builders and founders (joined at 250 members on the Slack channel).")
-            st.markdown("- 🚀‍ Launched [Humans of Builders club series](https://builder-club.beehiiv.com/), which interviews one builder about their incredibly inspiring build journeys.")
+            st.markdown("- 🚀‍ Launched [Humans of Builders club series](https://builder-club.beehiiv.com/), which interviews builders about their incredibly inspiring build journeys.")
 
             st.markdown("**Sep 2023**")
             st.markdown("- 📈 First 50 subscribers at [Fairylights newsletter](https://fairylightsai.substack.com/).")
