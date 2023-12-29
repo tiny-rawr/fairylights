a085b01f17dcf8f6fc58d56c408b7e91eae9180e
tiny_bear <rebeccawilliams9941@gmail.com>
2023-12-29 13:29:28 +1100

    :construction: Get API call to extract quotes to work.

--- projects/_002_interview_analyser/gpt_api_calls.py
+++ projects/_002_interview_analyser/gpt_api_calls.py
@@ -3,8 +3,9 @@ import json
 import streamlit as st
 from mixpanel import Mixpanel
 import re
+from demo_data.veteran_interview_donald_dugan import transcript
 
-mp = Mixpanel(st.secrets["mixpanel"]["token"])
+#mp = Mixpanel(st.secrets["mixpanel"]["token"])
 
 def create_key_name(text):
     text = re.sub(r'[^\x00-\x7F]+', '', text)
@@ -22,7 +23,7 @@ def generate_properties(questions):
 
     for question in questions:
         description = f"A direct quote from the interview that is relevant to '{question}'. Use direct quote only."
-        properties["interview"]["properties"][create_key_name(question)] = {
+        properties["interview"]["properties"][question] = {
             "type": "array",
             "items": {
                 "type": "string",
@@ -33,11 +34,12 @@ def generate_properties(questions):
     return properties
 
 def pull_quotes_from_transcript(questions, transcript):
-    api_key = st.session_state.api_key
+    #api_key = st.session_state.api_key
+    api_key = "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"
     client = OpenAI(api_key=api_key)
 
     conversation = [
-        {"role": "system", "content": f"You extract direct relevant quotes for the following transcript based on these: {questions}. This is the transcript"},
+        {"role": "system", "content": f"You extract direct relevant quotes for the following transcript based on these: {str(questions)}. This is the transcript"},
         {"role": "user", "content": transcript},
     ]
 
@@ -53,14 +55,61 @@ def pull_quotes_from_transcript(questions, transcript):
                     "type": "object",
                     "properties": generate_properties(questions),
                 },
-                "required": ["interview"],
+                "required": [question for question in questions],
             }
         }]
     )
 
-    response_message = response.choices[0].message
+    #response_message =  response.choices[0].message
+    return response
+    #tool_calls = response_message.tool_calls
+    #if tool_calls:
+        #return json.loads(response.choices[0].message.tool_calls[0].function.arguments)
+    #else:
+        #return {"interview": []}
+
+def pull_quotes_from_transcript(transcript, questions):
+    #api_key = st.session_state.api_key
+    api_key = "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"
+    client = OpenAI(api_key=api_key)
+
+    conversation = [
+        {"role": "system",
+         "content": "You extract direct quotes related to provided areas of interest, from the following transcript:"},
+        {"role": "user", "content": transcript},
+    ]
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=conversation,
+        tools=[{
+            "type": "function",
+            "function": {
+                "name": "extract_quotes",
+                "description": "Extracts quotes related to a provided area of interest.",
+                "parameters": {
+                    "type": "object",
+                    "properties": generate_properties(questions),
+                    "required": ["quotes"]
+                }
+            },
+        }],
+    )
+    response_message =  response.choices[0].message
     tool_calls = response_message.tool_calls
     if tool_calls:
-        return json.loads(response.choices[0].message.tool_calls[0].function.arguments)
+      return json.loads(response.choices[0].message.tool_calls[0].function.arguments)
     else:
-        return {"interview": []}
+      return {"interview": []}
+
+if __name__ == "__main__":
+    #print(transcript)
+    questions = ["Why did you join the military?"]
+    #print(generate_properties(questions))
+    transcript = "I'll never be good at eating cabbage. I love the color pink and my hat is pink. The world is big and blue. The carrot is red. The world is big and green."
+    #transcript = "I'll never be good at eating cabbage"
+    transcript = "Never have I ever said never"
+    questions = ["hat", "cabbage", "world"]
+    quotes = pull_quotes_from_transcript(transcript, questions)
+    print(quotes)
+    print(" ")
+
