99e94725b90d3c8024045fa768d00b84f410726f
tiny_bear <rebeccawilliams9941@gmail.com>
2024-01-12 22:03:50 +1100

    :construction: Add text-to-speech.

--- projects/_005_avatar_debate/gpt_api_calls.py
+++ projects/_005_avatar_debate/gpt_api_calls.py
@@ -11,7 +11,7 @@ def create_avatar_image(character_name, character_description):
 
     response = client.images.generate(
         model="dall-e-3",
-        prompt=f"Illustrate the following character in the style of Pixar Animation Studios: {character_name}. with a hint of a blurred natural background to suggest a sunny day. The character should have Pixar's signature appeal, including their approach to lighting and texture. Character description: {character_description}",
+        prompt=f"Illustrate the following character in the style of 3D Pixar Animation Studios: {character_name}, {character_description}. The character will face the viewer with a hint of a blurred natural background to suggest a sunny day. The overall effect is one of a three-dimensional appearance on a two-dimensional medium, with a careful balance between detail and softness that gives the character a lifelike presence.",
         size="1024x1024",
         quality="standard",
         n=1,
@@ -32,3 +32,24 @@ def create_avatar_image(character_name, character_description):
             return filename
 
     return "Failed to generate the image"
+
+def text_to_speech(gender="female", text="Hello, my name is Alina!"):
+    api_key = st.session_state.api_key
+    client = OpenAI(api_key=api_key)
+
+    gender = gender.lower()
+    voice = "shimmer"
+
+    if gender == "female":
+        voice = "nova"
+    elif gender == "male":
+        voice = "onyx"
+
+    speech_file_path = "projects/_005_avatar_debate/audio.wav"
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice=voice,
+        input=text
+    )
+
+    response.stream_to_file(speech_file_path)
--- projects/_005_avatar_debate/index.py
+++ projects/_005_avatar_debate/index.py
@@ -1,49 +1,56 @@
 import streamlit as st
 from projects._005_avatar_debate.gooey_api_calls import generate_talking_avatar
-from projects._005_avatar_debate.gpt_api_calls import create_avatar_image
+from projects._005_avatar_debate.gpt_api_calls import create_avatar_image, text_to_speech
+
 
 def avatar_debate():
     st.title("Chatty Character")
-    st.write("Chat to a custom character. Record or type a message, and your avatar will chat back to you (speech, lip syncing, emotions and gestures).")
+    st.write(
+        "Chat to a custom character. Record or type a message, and your avatar will chat back to you (speech, lip syncing, emotions and gestures).")
 
-    audio = "projects/_005_avatar_debate/audio.wav"
     form_input = st.empty()
 
     with form_input.form("Character Information"):
         character_name = st.text_input("Character Name", value="Alina")
-
         age = st.number_input("Age", min_value=0, value=29)
-        gender = st.radio("Gender", ["Male", "Female", "Other"])
-        eye_color = st.text_input("Eye Color", value="Green")
+        gender = st.radio("Gender", ["Female", "Male", "Other"])
+        eye_color = st.text_input("Eye Color", value="Blue")
         hair_description = st.text_input("Hair Description", value="Long curly red hair")
         clothing_description = st.text_input("Clothing Description",
-                                             value="Deep green spaghetti strap dress with sparkly sequins")
+                                             value="Deep green hoodie with a cute dinosaur on the front")
 
-        # Add a submit button to the form
+        character_description = f"Age: {age} years old.\nGender: {gender}.\nEyes: {eye_color} eyes.\nHair: {hair_description}.\nClothing: {clothing_description}"
         submit_button = st.form_submit_button("Generate Character")
 
     character = st.empty()
     progress = st.empty()
 
     if submit_button:
-        character_description = f"Age: {age} years old.\nGender: {gender}.\nEyes: {eye_color} eyes.\nHair: {hair_description}.\nClothing: {clothing_description}"
-
         api_key = st.session_state.get('api_key', '')
 
         if not api_key:
             st.error("🔐  Please enter an OpenAI API key in the sidebar to proceed.")
             return
 
-        progress.info("1/2) Generating character image with GPT-Vision...")
+        progress.info("1/3) Generating character image with GPT-Vision...")
         character_image = create_avatar_image(character_name, character_description)
 
-        progress.info("2/2) Generating Lip Syncing character with Gooey.AI")
+        progress.info("2/3) Generating Audio from response")
+        text_to_speech(gender, f"Hello, my name is {character_name}!")
+        audio = "projects/_005_avatar_debate/audio.wav"
+
+        progress.info("3/3) Generating Lip Syncing character with Gooey.AI")
         avatar_url = generate_talking_avatar(character_image, audio)
         character.video(avatar_url)
         progress.empty()
         form_input.empty()
 
 
+    #with st.form("Messenger"):
+        #message = st.input_area("Write a message")
+        #submit = st.submit_button("Send message")
+
+        #if submit("")
 
 if __name__ == "__main__":
     avatar_debate()
