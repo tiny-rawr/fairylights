d94fa6b8903e7be87be7b0383bd69961b8f2adc0
tiny_bear <rebeccawilliams9941@gmail.com>
2024-02-14 17:57:55 +1100

    :construction: Minor change.

--- projects/_005_avatar_debate/gpt_api_calls.py
+++ projects/_005_avatar_debate/gpt_api_calls.py
@@ -66,6 +66,3 @@ def text_to_speech(gender="female", text="Hello, my name is Alina!"):
     )
 
     response.stream_to_file(speech_file_path)
-
-if __name__ == "__main__":
-    #print(respond_to_message("Hello"))
